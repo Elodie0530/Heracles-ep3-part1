96d78b08456b9c08957bb2f3e6719d426cb37c99
Elodie0530 <uneondine@gmail.com>
2023-04-20 12:19:01 +0200

    exercice créer plusieurs combattants et les poser sur une carte

---
+++ src/Monster.js
@@ -0,0 +1,13 @@
+class Monster extends Fighter {
+  constructor(name, strength, dexterity, image, x, y) {
+    super(name, strength, dexterity, image, x, y);
+  }
+
+  getDamage() {
+    return this.strength;
+  }
+
+  getDefense() {
+    return this.dexterity;
+  }
+}
